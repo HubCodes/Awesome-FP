08b20186d165b2698db3c9ab02586936fb66b4f5
HubCodes <krlrhkstk12@gmail.com>
2019-04-14 13:35:45 +0900

    bug fix on doubleEveryOther and completed problem 1

--- CIS 194/01-intro/homework/validatingCreditCard.hs
+++ CIS 194/01-intro/homework/validatingCreditCard.hs
@@ -11,12 +11,20 @@ toDigitsRev digit = reverse (toDigits digit)
 
 -- Exercise 2
 
+doubleEveryOtherReversed :: [Integer] -> [Integer]
+doubleEveryOtherReversed [] = []
+doubleEveryOtherReversed (x:[]) = [x]
+doubleEveryOtherReversed (x:ys) = [x] ++ [(head ys) * 2] ++ doubleEveryOtherReversed (tail ys)
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] = []
-doubleEveryOther (x:[]) = [x]
-doubleEveryOther (x:ys) = [x] ++ [head ys * 2] ++ doubleEveryOther (tail ys)
+doubleEveryOther list = reverse (doubleEveryOtherReversed (reverse list))
 
 -- Exercise 3
 
 sumDigits :: [Integer] -> Integer
 sumDigits digits = sum (concat (map toDigits digits))
+
+-- Exercise 4
+
+validate :: Integer -> Bool
+validate card = sumDigits (doubleEveryOther (toDigits card)) `mod` 10 == 0
